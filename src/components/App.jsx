b37c6942780ffcb3034e1c40eaa18903ec617f47
angelkapv81@gmail.com <angelkapv81>
2023-05-12 23:40:32 +0300

    fix component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import React from 'react';
+import { GlobalStyle } from './GlobalStyle';
+// import Button from './Button/Button';
+// import ImageGallery from './ImageGallery/ImageGallery';
+// import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import Loader from './Loader/Loader';
+// import Modal from './Modal/Modal';
+// import Searchbar from './Searchbar/Searchbar';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +16,18 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      {/* <Button /> */}
+      {/* <ImageGallery /> */}
+      {/* <ImageGalleryItem /> */}
+      <Loader />
+      {/* <Modal /> */}
+      {/* <Searchbar /> */}
+      <GlobalStyle />
     </div>
   );
 };
+export default App;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+import { ContactsBlock } from './Loader.styled';
+import { Component } from 'react';
+
+const Loader = ({}) => {
+  return <ContactsBlock></ContactsBlock>;
+};
+
+export default Loader;
